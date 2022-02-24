280cd67c1869c652a0986a2725a85e5e368e890e
lokeshrocks <92288135+lokeshrocks@users.noreply.github.com>
2022-02-24 14:39:21 +0530

    Add files via upload

---
+++ Number1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Number1
+{
+    public static void main(String args[])
+    {
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter n value");
+    int n=s.nextInt();  
+    int i=0;
+    for(i=n;i>=1;i--)
+        {
+        System.out.print(i+" ");
+        }                        
+    }
+}

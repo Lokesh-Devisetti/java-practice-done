faaa1324fbece3c93109a69eb0915306cf042b5e
lokeshrocks <92288135+lokeshrocks@users.noreply.github.com>
2022-02-25 18:17:56 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,12 @@
+public class Sum
+{
+    public static void main(String args[]){
+        int arr[]={8,4,5,2,8,7,9};
+        int su=0;
+        for(int i=0;i<arr.length;i++){
+            su=su+arr[i];
+        }
+            System.out.println("sum of given array"+su);
+        
+    }
+}
